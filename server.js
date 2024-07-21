4ed09fa16fab93f3cab3d10b5ef7e09a655bd1ef
MaresR0bert <robi_mares@yahoo.com>
2024-07-21 12:58:57 +0300

    Add file read

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/:stock/:number', (req, res) => {
 
     const stock = req.params.stock;
     var numberOfFiles = req.params.number;
-    // var selectedFiles = [];
+    var selectedFiles = [];
 
     /**
      * Read all files in stock directory
@@ -27,7 +27,27 @@ app.get('/:stock/:number', (req, res) => {
          * If there aren’t enough files present for a given exchange, process whatever number of files are present even if it is lower.
          */
         numberOfFiles = numberOfFiles > foundFiles.length ? foundFiles.length : numberOfFiles;
-        selectedFiles = foundFiles.slice(0, numberOfFiles);
+
+        // Get the exact number of files and remove extention
+        selectedFiles = foundFiles.slice(0, numberOfFiles).map(elem => elem = elem.split('.')[0]);
+
+        // Declare matrix to store data
+        const readData = [];
+        selectedFiles.forEach(() => readData.push([]));
+
+        selectedFiles.forEach((elem, index) => {
+            fileSystem.createReadStream('./'+ stock + '/' + elem +'.csv')
+            .pipe(csvParser(['StockID', 'Timestamp', 'Price']))
+            .on('data', (row) => {
+                readData[index].push(row);
+            })
+            .on('end', () => {
+                const randomElementIndex = Math.floor(Math.random() * (readData[index].length - 10));
+                const valuesToForecat = readData[index].slice(randomElementIndex, randomElementIndex + 10);
+                console.log(valuesToForecat);
+            })
+        })
+
         return res.send('Stock: ' + stock + " Number: " + numberOfFiles);
     });
 });
