3f69c08ee4cca678ef83c461a0757218a4d9ece1
MaresR0bert <robi_mares@yahoo.com>
2024-07-21 12:37:56 +0300

    Add file processing and filtering

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 // app.js
 const express = require('express');
+const fileSystem = require('fs');
+const csvParser = require('csv-parser');
 const app = express();
 
 app.get('/:stock/:number', (req, res) => {
 
     const stock = req.params.stock;
-    var number = req.params.number;
+    var numberOfFiles = req.params.number;
+    // var selectedFiles = [];
 
-    res.send("Stock: " + stock + ' Number: '+ number);
+    /**
+     * Read all files in stock directory
+     */
+    fileSystem.readdir('./'+ stock, (error, foundFiles) => {
+        if(error) {
+            // return alert pop-up if stock does not exist
+            return res.send(`<script> alert("Stock ${stock} was not found") </script>`);
+        }
+
+        // Filter files in stock directory to only fetch .csv files
+        foundFiles = foundFiles.filter(file => file.endsWith('.csv'));
+
+        /**
+         * Citetion from Requirements:
+         * If there aren’t enough files present for a given exchange, process whatever number of files are present even if it is lower.
+         */
+        numberOfFiles = numberOfFiles > foundFiles.length ? foundFiles.length : numberOfFiles;
+        selectedFiles = foundFiles.slice(0, numberOfFiles);
+        return res.send('Stock: ' + stock + " Number: " + numberOfFiles);
+    });
 });
 
 // Start the server
